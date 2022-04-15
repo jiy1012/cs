7d028c617be9ef763c4f9cdfb74bcce43a3d3755
yi.liu <yi.liu@huolala.cn>
2022-04-15 16:11:58 +0800

    添加版本信息

--- ArgStruct.go
+++ ArgStruct.go
@@ -7,3 +7,7 @@ type Args struct {
 	AutoAddPerfix bool   `arg:"-a" default:"false" placeholder:"自动添加前缀，解决重名问题。默认为false，如无法确保是否有重名变量，可设置为true"`
 	GoRoot        string `arg:"-g,env:GOROOT" placeholder:"go安装目录，取环境变量GOROOT"`
 }
+
+func (Args) Version() string {
+	return "cs 0.0.1"
+}
